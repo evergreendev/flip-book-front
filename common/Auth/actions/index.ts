c7beb0b72ad3d22670d572dacd3af3421b0b2078
Joe simpson <joe@egmrc.com>
2025-01-22 12:07:45 -0700

    Add dummy log in log out functionality

--- common/Auth/actions/index.ts
+++ common/Auth/actions/index.ts
@@ -1,22 +1,32 @@
+"use server"
 import {cookies} from "next/headers";
 import {redirect} from "next/navigation";
 
 const userTokenKey = "user_token";
 const dashboardUrl = "/admin/dashboard";
 
-export async function handleLogin() {
-    "use server"
+export async function handleLogin(prevState: {error?:string|null, redirect?:boolean|null}, formData: FormData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (!process.env.BACKEND_URL) return { error: "Invalid Credentials. Please Try again" };
+
+    const res = await fetch(process.env.BACKEND_URL+"/session", {
+        method: "POST",
+        body: JSON.stringify({email, password}),
+    });
+    const data = await res.json();
+
     const cookieStore = await cookies();
-    cookieStore.set(userTokenKey, 'testtoken', {
+    cookieStore.set(userTokenKey, data[userTokenKey], {
         secure: process.env.NODE_ENV === "production",
         sameSite: true,
     });
 
-    redirect(dashboardUrl);
+    return { error: null, redirect: true}
 }
 
 export async function handleLogout() {
-    "use server"
     const cookieStore = await cookies();
     cookieStore.delete(userTokenKey);
 
