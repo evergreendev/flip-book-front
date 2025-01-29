f88436a5becc84562bf429a0594d900814ed3965
Joe simpson <joe@egmrc.com>
2025-01-29 09:43:47 -0700

    Add authorization to upload

--- app/(admin)/admin/(protected)/dashboard/edit/actions/upload.ts
+++ app/(admin)/admin/(protected)/dashboard/edit/actions/upload.ts
@@ -1,12 +1,23 @@
 "use server"
 
+import {cookies} from "next/headers";
+
 export async function handleUpload(prevState: { error?: string | null, redirect?: boolean | null }, formData: FormData) {
     if (!process.env.BACKEND_URL) return {...prevState, error: "Something went wrong. Please try again."};
+
+    const cookieStore = await cookies();
+    const userToken = cookieStore.get('user_token');
 
     const res = await fetch(process.env.BACKEND_URL + "/pdf/upload", {
         method: "POST",
         body: formData,
+        headers: {
+            "Authorization": `Bearer ${userToken?.value}`
+        }
     });
+    if (res.status === 401) {
+        cookieStore.delete('user_token');
+    }
     if (res.status !== 200) return {...prevState, error: "Invalid Credentials. Please Try again"};
 
     return {error: null, redirect: true}
