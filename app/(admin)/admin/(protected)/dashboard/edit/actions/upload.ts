e11d892eb7da04c15fde85aff7980fdb5a253106
Joe simpson <joe@egmrc.com>
2025-02-04 16:44:56 -0700

    Update pdf form to create a flipbook on upload

--- app/(admin)/admin/(protected)/dashboard/edit/actions/upload.ts
+++ app/(admin)/admin/(protected)/dashboard/edit/actions/upload.ts
@@ -2,7 +2,7 @@
 
 import {cookies} from "next/headers";
 
-export async function handleUpload(prevState: { error?: string | null, redirect?: boolean | null }, formData: FormData) {
+export async function handleUpload(prevState: { error?: string | null, redirect?: string | null }, formData: FormData) {
     if (!process.env.BACKEND_URL) return {...prevState, error: "Something went wrong. Please try again."};
 
     const cookieStore = await cookies();
@@ -20,5 +20,21 @@ export async function handleUpload(prevState: { error?: string | null, redirect?
     }
     if (res.status !== 200) return {...prevState, error: "Invalid Credentials. Please Try again"};
 
-    return {error: null, redirect: true}
+    const pdf = await res.json();
+
+    const flipbookRes = await fetch(process.env.BACKEND_URL + "/flipbooks", {
+        method: "POST",
+        body: JSON.stringify({
+            pdfPath: pdf.filePath,
+            status: "draft",
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${userToken?.value}`
+        }
+    });
+    if (res.status !== 200) return {...prevState, error: "Invalid Credentials. Please Try again"};
+    const flipbookBody = await flipbookRes.json();
+
+    return {error: null, redirect: flipbookBody.flipbook.id}
 }
