2fcb18b38e6f300de7c69353264013e2206ae5b3
Joe simpson <joe@egmrc.com>
2025-02-05 16:18:36 -0700

    Add flipbook table

--- app/(admin)/admin/(protected)/components/Header.tsx
+++ app/(admin)/admin/(protected)/components/Header.tsx
@@ -3,7 +3,7 @@ import {handleLogout} from "@/app/common/Auth/actions";
 import Link from "next/link";
 
 const Header = () => {
-    return <div className="w-full bg-slate-800 p-4 flex items-center justify-center gap-2">
+    return <div className="w-full bg-slate-800 text-white p-4 flex items-center justify-center gap-2">
         <Link className="border-white border px-1" href="/admin/dashboard/register">Add User</Link>
         <Link className="border-white border px-1" href="/admin/dashboard/edit">Create New Flip Book</Link>
         <LogoutButton handleLogout={handleLogout}>
---
+++ app/(admin)/admin/(protected)/dashboard/flipbooks/page.tsx
@@ -0,0 +1,31 @@
+import { FlipBook, columns } from "./columns"
+import { DataTable } from "./data-table"
+import {cookies} from "next/headers";
+
+async function getData(): Promise<FlipBook[]> {
+    const cookieStore = await cookies();
+    const userToken = cookieStore.get('user_token');
+    const flipbookRes = await fetch(process.env.BACKEND_URL + "/flipbooks?showDrafts=true", {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${userToken?.value}`
+        }
+    });
+    const data = await flipbookRes.json();
+
+    return (data as FlipBook[]).map((row) => {
+        return {...row,
+            title: row.title || "Unnamed Flipbook"
+        }
+    });
+}
+
+export default async function DemoPage() {
+    const data = await getData()
+
+    return (
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+        </div>
+    )
+}
