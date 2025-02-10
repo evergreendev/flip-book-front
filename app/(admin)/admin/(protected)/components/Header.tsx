83017e0ae49c295738612965cffc03faf11a3b29
Joe simpson <joe@egmrc.com>
2025-02-10 15:37:04 -0700

    Add view flipbook button

--- app/(admin)/admin/(protected)/components/Header.tsx
+++ app/(admin)/admin/(protected)/components/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 const Header = () => {
     return <div className="w-full bg-slate-800 text-white p-4 flex items-center justify-center gap-2">
         <Link className="border-white border px-1" href="/admin/dashboard/register">Add User</Link>
+        <Link className="border-white border px-1" href="/admin/dashboard/flipbooks">View Flipbooks</Link>
         <Link className="border-white border px-1" href="/admin/dashboard/edit">Create New Flip Book</Link>
         <LogoutButton handleLogout={handleLogout}>
             Log Out
