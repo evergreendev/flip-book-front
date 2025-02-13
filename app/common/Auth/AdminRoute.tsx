601ef285dac90b46d9f691b9bee49087504abcbc
Joe simpson <joe@egmrc.com>
2025-02-13 11:26:10 -0700

    Update admin route to check if token is expired and if token has admin claim

--- app/common/Auth/AdminRoute.tsx
+++ app/common/Auth/AdminRoute.tsx
@@ -2,11 +2,21 @@
 import { cookies } from 'next/headers'
 import {redirect} from "next/navigation";
 
+async function validateToken(token: string): Promise<boolean> {
+    const res = await fetch(`${process.env.BACKEND_URL}/session/validate/${token}`, {});
+    if (!res.ok) return false;
+
+    const data = await res.json();
+
+    return data["user_token"].isAdmin;
+}
+
 const AdminRoute = async ({children}: { children: React.ReactNode }) => {
     const cookieStore = await cookies();
-    const userToken = cookieStore.get('user_token');
+    const userToken = cookieStore.get('user_token')?.value;
+    const tokenIsValid = await validateToken(userToken||"");
 
-    if (!userToken) {
+    if (!userToken || !tokenIsValid) {
         redirect('/admin');
     }
 
