953640d0e4431f2e6397b3a996af588acbf0d1a9
Joe simpson <joe@egmrc.com>
2025-11-10 16:49:50 -0700

    Add console.log

--- app/common/Sessions/actions/index.ts
+++ app/common/Sessions/actions/index.ts
@@ -9,5 +9,9 @@ export async function handleCreateSession() {
         headers: {"Content-Type": "application/json"}
     });
 
+    const data = await res.json();
+
+    console.log(data);
+
     return await res.json();
 }
